cd96f340915fdb33fa42626dc3518fb17a08653c
wrobell <wrobell@pld-linux.org>
2001-08-13 15:48:34 +0000

    - updated to fop 0.20.0
    Changed files:
        fop.sh -> 1.2

--- fop.sh
+++ fop.sh
@@ -5,7 +5,7 @@ FOP_CONF=$FONTDIR/fop-font.config
 
 CLASSPATH=/usr/share/java/classes/fop.jar
 CLASSPATH=$CLASSPATH:/usr/share/java/classes/jimi-1.0.jar
-CLASSPATH=$CLASSPATH:/usr/share/java/classes/w3c.jar
+CLASSPATH=$CLASSPATH:/usr/share/java/classes/batik.jar
 CLASSPATH=$CLASSPATH:/usr/share/java/classes/xalan-2.0.0.jar
 CLASSPATH=$CLASSPATH:/usr/share/java/classes/xerces-1.2.3.jar
 
