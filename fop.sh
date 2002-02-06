f2d568c0fab7421032fba64705c7e3374314dd0a
wrobell <wrobell@pld-linux.org>
2002-02-06 18:52:55 +0000

    - use /usr/lib/java - use system installed *.jar files
    Changed files:
        fop.sh -> 1.3

--- fop.sh
+++ fop.sh
@@ -3,11 +3,10 @@
 FONTDIR=/usr/share/fonts
 FOP_CONF=$FONTDIR/fop-font.config
 
-CLASSPATH=/usr/share/java/classes/fop.jar
-CLASSPATH=$CLASSPATH:/usr/share/java/classes/jimi-1.0.jar
-CLASSPATH=$CLASSPATH:/usr/share/java/classes/batik.jar
-CLASSPATH=$CLASSPATH:/usr/share/java/classes/xalan-2.0.0.jar
-CLASSPATH=$CLASSPATH:/usr/share/java/classes/xerces-1.2.3.jar
+CLASSPATH=/usr/lib/java/fop.jar
+CLASSPATH=$CLASSPATH:/usr/lib/java/jimi-1.0.jar
+CLASSPATH=$CLASSPATH:/usr/lib/java/batik.jar
+CLASSPATH=$CLASSPATH:/usr/lib/java/xalan.jar
+CLASSPATH=$CLASSPATH:/usr/lib/java/xerces.jar
 
-/usr/lib/java-jre/jre/bin/java -classpath $CLASSPATH \
-    org.apache.fop.apps.Fop -c $FOP_CONF $@
+java -classpath $CLASSPATH org.apache.fop.apps.Fop -c $FOP_CONF $@
