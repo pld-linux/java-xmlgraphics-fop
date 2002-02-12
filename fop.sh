cb033aad5c3a52121a7f591d6342b4b372e97187
wrobell <wrobell@pld-linux.org>
2002-02-12 10:38:37 +0000

    - batik jar path fixed
    Changed files:
        fop.sh -> 1.6

--- fop.sh
+++ fop.sh
@@ -9,8 +9,8 @@ CLASSPATH=$JCL/fop.jar
 CLASSPATH=$CLASSPATH:$JCL/jimi-1.0.jar
 CLASSPATH=$CLASSPATH:$JCL/logkit-1.0b4.jar
 CLASSPATH=$CLASSPATH:$JCL/avalon-framework-4.0.jar
-CLASSPATH=$CLASSPATH:$JCL/batik.jar
 CLASSPATH=$CLASSPATH:$JCL/xalan.jar
 CLASSPATH=$CLASSPATH:$JCL/xerces.jar
+CLASSPATH=$CLASSPATH:$JCL/batik/batik.jar
 
 java -classpath $CLASSPATH org.apache.fop.apps.Fop -c $FOP_CONF $@
