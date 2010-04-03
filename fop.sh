c2f25f2b741d499f89ba6127d050e1a6cf8c517d
pawelz <pawelz@pld-linux.org>
2010-04-03 21:24:42 +0000

    - fixed classpath - new main class
    Changed files:
        fop.sh -> 1.8

--- fop.sh
+++ fop.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/sh -e
 
 FONTDIR=/usr/share/fonts
 FOP_CONF=$FONTDIR/fop-font.config
 
-jars="fop avalon-framework xalan xerces-j2 batik"
+jars="xmlgraphics-fop avalon-framework-api avalon-framework-impl commons-io xalan xerces-j2 xmlgraphics-batik xmlgraphics-batik/batik-util"
 CLASSPATH=$(build-classpath $jars)
 
-exec java -classpath $CLASSPATH org.apache.fop.apps.Fop -c $FOP_CONF $@
+exec java -classpath $CLASSPATH org.apache.fop.cli.Main -c $FOP_CONF $@
