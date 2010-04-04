118e890901a4a0d151fb89d71e10b0f37d0de54f
pawelz <pawelz@pld-linux.org>
2010-04-04 19:57:50 +0000

    - by default use config from /var/lib/fop - don't override FOP_CONFIG env if one specified - don't override -c cmdline option if one specified
    Changed files:
        fop.sh -> 1.9

--- fop.sh
+++ fop.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -e
 
-FONTDIR=/usr/share/fonts
-FOP_CONF=$FONTDIR/fop-font.config
+FOPDIR=/var/lib/fop
+FOP_CONF=${FOP_CONF:-"$FOPDIR/fop-font.config"}
 
 jars="xmlgraphics-fop avalon-framework-api avalon-framework-impl commons-io xalan xerces-j2 xmlgraphics-batik xmlgraphics-batik/batik-util"
 CLASSPATH=$(build-classpath $jars)
 
-exec java -classpath $CLASSPATH org.apache.fop.cli.Main -c $FOP_CONF $@
+# If user specified -c option, and it is not the last option (i.e. it has
+# an argument), don't override it.
+case " $*" in
+  *' -c '*) FOP_CONFIG_OPTION="" ;;
+  *) FOP_CONFIG_OPTION="-c $FOP_CONFIG" ;;
+esac
+
+exec java -classpath $CLASSPATH org.apache.fop.cli.Main $CONFIG $@
