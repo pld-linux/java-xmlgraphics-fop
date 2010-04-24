d7d8faec48f6ccdc88e585efa7329f74919de086
pawelz <pawelz@pld-linux.org>
2010-04-24 22:03:58 +0000

    - use jpackage-utils
    Changed files:
        fop.sh -> 1.10

--- fop.sh
+++ fop.sh
@@ -1,10 +1,15 @@
 #!/bin/sh -e
 
+[ -r /usr/share/java-utils/java-functions ] || exit 1
+
+. /usr/share/java-utils/java-functions
+
 FOPDIR=/var/lib/fop
 FOP_CONF=${FOP_CONF:-"$FOPDIR/fop-font.config"}
 
 jars="xmlgraphics-fop avalon-framework-api avalon-framework-impl commons-io xalan xerces-j2 xmlgraphics-batik xmlgraphics-batik/batik-util"
 CLASSPATH=$(build-classpath $jars)
+MAIN_CLASS=org.apache.fop.cli.Main
 
 # If user specified -c option, and it is not the last option (i.e. it has
 # an argument), don't override it.
@@ -13,4 +18,4 @@ case " $*" in
   *) FOP_CONFIG_OPTION="-c $FOP_CONFIG" ;;
 esac
 
-exec java -classpath $CLASSPATH org.apache.fop.cli.Main $CONFIG $@
+run $CONFIG ${1:+"$@"}
